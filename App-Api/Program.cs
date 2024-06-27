a15114801dd3b1a4500959a8c37bf032b6775ab3
HeL <aqhelaja@gmail.com>
2024-06-28 00:00:00 +0700

    Update: App-Api/Program.cs

--- App-Api/Program.cs
+++ App-Api/Program.cs
@@ -1,3 +1,5 @@
+using Fearlessforever.Api.Modules.Sample;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//========================================================
+// Register Module Service & Configuration
+builder.Services.AddModuleSampleServices();
+//========================================================
+
 
 var app = builder.Build();
 
