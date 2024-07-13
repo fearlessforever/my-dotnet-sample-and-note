ad42eea2be7a65c8b0727b6c49b5159940919475
HeL <aqhelaja@gmail.com>
2024-07-14 00:00:00 +0700

    Add: Shared/Configs/FeaturesConfig.cs

---
+++ Shared/Configs/FeaturesConfig.cs
@@ -0,0 +1,8 @@
+
+namespace Fearlessforever.Shared.Configs;
+
+public class FeaturesConfig
+{
+  public bool UseRedisCache { get; set; } = false;
+  public bool UseDatabase { get; set; } = false;
+}
