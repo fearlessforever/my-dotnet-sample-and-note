0b651e5bec3843b6bc3834eaf204586d1aaa5604
HeL <aqhelaja@gmail.com>
2024-08-11 00:00:00 +0700

    Update: Shared/Configs/FeaturesConfig.cs

--- Shared/Configs/FeaturesConfig.cs
+++ Shared/Configs/FeaturesConfig.cs
@@ -3,8 +3,12 @@ namespace Fearlessforever.Shared.Configs;
 
 public class FeaturesConfig
 {
+  public bool ApplyDatabasesMigrationsAndSeederWhenAppStart { get; set; } = false;
   public bool UseRedisCache { get; set; } = false;
-  public bool UseDatabase { get; set; } = false;
+  public bool UseDatabaseSqlite { get; set; } = false;
+  public bool UseDatabaseInMemory { get; set; } = false;
+  public bool UseDatabaseMsSql { get; set; } = false;
+  public bool UseDatabasePostgreSql { get; set; } = false;
   public bool UseQueueHangFire { get; set; } = false;
   public bool UseSignalR { get; set; } = false;
 }
