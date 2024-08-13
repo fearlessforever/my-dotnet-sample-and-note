1dfd512db656b4cd360b249ac84702c29440e02d
HeL <aqhelaja@gmail.com>
2024-08-14 00:00:00 +0700

    Add: Databases/Contexts/Configurations/MsSql/BookConfig.cs

---
+++ Databases/Contexts/Configurations/MsSql/BookConfig.cs
@@ -0,0 +1,19 @@
+
+
+using Fearlessforever.Databases.Shared.Models;
+using Fearlessforever.Shared.Utils;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Fearlessforever.Databases.Contexts.Configurations.MsSql;
+public class BookConfig : IEntityTypeConfiguration<Book>
+{
+  public void Configure(EntityTypeBuilder<Book> builder)
+  {
+    builder.ToTable("Books" , schema: "CityLibrary" );
+    builder.HasKey(x => x.Id);
+    builder.Property(x => x.DateCreated).HasDefaultValueSql(MyConstants.CurrentTimeStampMsSql);
+    builder.HasQueryFilter(x => x.IsDeleted == false || x.IsDeleted == null);
+    builder.HasIndex(x => x.IsDeleted);
+  }
+}
