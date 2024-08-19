c3ca32dd543647fc26e804abe09c5cf1ccfc4512
HeL <aqhelaja@gmail.com>
2024-08-20 00:00:00 +0700

    Add: Databases/Migrations/AppSqlite/20251115045903_InitiateProject.cs

---
+++ Databases/Migrations/AppSqlite/20251115045903_InitiateProject.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fearlessforever.Databases.Migrations.AppSqlite
+{
+    /// <inheritdoc />
+    public partial class InitiateProject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    EmailAddress = table.Column<string>(type: "TEXT", nullable: false),
+                    Address = table.Column<string>(type: "TEXT", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    DateUpdated = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    CreatedBy = table.Column<string>(type: "TEXT", maxLength: 255, nullable: true),
+                    UpdatedBy = table.Column<string>(type: "TEXT", maxLength: 255, nullable: true),
+                    IsDeleted = table.Column<bool>(type: "INTEGER", nullable: true),
+                    DeletedBy = table.Column<string>(type: "TEXT", maxLength: 255, nullable: true),
+                    DateDeleted = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Contents = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    AuthorId = table.Column<int>(type: "INTEGER", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    DateUpdated = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    CreatedBy = table.Column<string>(type: "TEXT", maxLength: 255, nullable: true),
+                    UpdatedBy = table.Column<string>(type: "TEXT", maxLength: 255, nullable: true),
+                    IsDeleted = table.Column<bool>(type: "INTEGER", nullable: true),
+                    DeletedBy = table.Column<string>(type: "TEXT", maxLength: 255, nullable: true),
+                    DateDeleted = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Authors_IsDeleted",
+                table: "Authors",
+                column: "IsDeleted");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_IsDeleted",
+                table: "Blogs",
+                column: "IsDeleted");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.DropTable(
+                name: "Blogs");
+        }
+    }
+}
