c671d803320cb8f0f4fa99d3547164a037989dd4
HeL <aqhelaja@gmail.com>
2024-09-03 00:00:00 +0700

    Update: App-Api/Core/FluentValidation/MyValidationFilter.cs

--- App-Api/Core/FluentValidation/MyValidationFilter.cs
+++ App-Api/Core/FluentValidation/MyValidationFilter.cs
@@ -9,10 +9,19 @@ public class MyValidationFilter<T>(IValidator<T> validator) : IEndpointFilter
   // public async ValueTask<object?> In
   public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
   {
-    var request = context.Arguments.OfType<T>().First();
-
-    var result = await validator.ValidateAsync(request);
-
-    return result.IsValid ? await next(context) : MyApiResponse.GenerateApiResponse<object>(null, message:"Bad Request", status: "error" , isHeaderStatus: true , code:400, errors: result.ToDictionary() );
+    try
+    {
+      var request = context.Arguments.OfType<T>().First();
+      var result = await validator.ValidateAsync(request, context.HttpContext.RequestAborted);
+      return result.IsValid ? await next(context) : MyApiResponse.GenerateApiResponse<object>(null, message:"Bad Request", status: "error" , isHeaderStatus: true , code:400, errors: result.ToDictionary() );
+    }
+    catch (InvalidOperationException ex)
+    {
+      Type typeValidator = typeof(T);
+      IEnumerable<string> errorResult = [];
+      errorResult = errorResult.Prepend( ex.Message );
+      errorResult = errorResult.Prepend($"Filter Validator: {typeValidator.Name}");
+      return MyApiResponse.GenerateApiResponse<object>(null, message: "Bad Request", status: "error", isHeaderStatus: true, code: 500, errors: errorResult);
+    }
   }
 }
