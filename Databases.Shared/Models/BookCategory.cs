ca66c0d1a61cf8a05f03324d138e00a3ee5911d4
HeL <aqhelaja@gmail.com>
2025-03-22 00:00:00 +0700

    Update: Databases.Shared/Models/BookCategory.cs

--- Databases.Shared/Models/BookCategory.cs
+++ Databases.Shared/Models/BookCategory.cs
@@ -6,7 +6,9 @@ namespace Fearlessforever.Databases.Shared.Models;
 public class BookCategory : BaseEntitySoftDelete<int>
 {
   public required string Name { get; set; }
-  
+
   [ForeignKey("BookId")]
   public required int? BookId { get; set; }
+  
+  public Book? Book { get; set; }
 }
