5f1507c9be12d0d9ee1d9f6d628cfec771d2de80
HeL <aqhelaja@gmail.com>
2025-03-23 00:00:00 +0700

    Update: Databases/Contexts/Configurations/MsSql/BookConfig.cs

--- Databases/Contexts/Configurations/MsSql/BookConfig.cs
+++ Databases/Contexts/Configurations/MsSql/BookConfig.cs
@@ -10,10 +10,12 @@ public class BookConfig : IEntityTypeConfiguration<Book>
 {
   public void Configure(EntityTypeBuilder<Book> builder)
   {
-    builder.ToTable("Books" , schema: "CityLibrary" );
+    builder.ToTable("Books", schema: "CityLibrary");
     builder.HasKey(x => x.Id);
     builder.Property(x => x.DateCreated).HasDefaultValueSql(MyConstants.CurrentTimeStampMsSql);
     builder.HasQueryFilter(x => x.IsDeleted == false || x.IsDeleted == null);
     builder.HasIndex(x => x.IsDeleted);
+
+    builder.HasMany(x => x.BookCategories).WithOne(x => x.Book);
   }
 }
