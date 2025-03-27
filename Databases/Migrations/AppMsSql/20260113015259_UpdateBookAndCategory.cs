18b9f27cc21fa82239ccfa32b7e01e419bb8b952
HeL <aqhelaja@gmail.com>
2025-03-28 00:00:00 +0700

    Add: Databases/Migrations/AppMsSql/20260113015259_UpdateBookAndCategory.cs

---
+++ Databases/Migrations/AppMsSql/20260113015259_UpdateBookAndCategory.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fearlessforever.Databases.Migrations.AppMsSql
+{
+    /// <inheritdoc />
+    public partial class UpdateBookAndCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_BookCategories_BookId",
+                schema: "CityLibrary",
+                table: "BookCategories",
+                column: "BookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookCategories_Books_BookId",
+                schema: "CityLibrary",
+                table: "BookCategories",
+                column: "BookId",
+                principalSchema: "CityLibrary",
+                principalTable: "Books",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookCategories_Books_BookId",
+                schema: "CityLibrary",
+                table: "BookCategories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BookCategories_BookId",
+                schema: "CityLibrary",
+                table: "BookCategories");
+        }
+    }
+}
